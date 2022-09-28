9e7133d7956d56d2a375af9f1e2037f78b86f848
Likhith329 <likhithmattapalli@gmail.com>
2022-09-29 01:32:21 +0530

    first commit

--- script.js
+++ script.js
@@ -1,25 +1,38 @@
-async function fun1(){
-    let urldata= await fetch("https://random.dog/woof.json")
-    let fetcheddata=await urldata.json()
-    try {
-        let imagedata=document.querySelector(".image")
-    imagedata.setAttribute("src",fetcheddata.url)
-    } catch (error) {
-        let videodata=document.querySelector(".video")
-        videodata.setAttribute("src",fetcheddata.url)
-    }
+
+async function fun(){
+  try {
+    let inpdat=document.querySelector("input[type='text']").value
+
+  let fetcheddata=await fetch("https://api.dictionaryapi.dev/api/v2/entries/en/"+inpdat)
+
+  let data= await fetcheddata.json()
+  
+ 
+
+  let infdata=document.querySelector(".info")
+
+  let inf=''
+  
+  data[0].meanings.forEach(partsofspeech => {
+    let k=0
+    inf+=`
+    <div class="content">
+    <h5 class="pts">${partsofspeech.partOfSpeech}</h5>
+    <div>
+     <div class="meaning">${k++,((partsofspeech.definitions[0])?partsofspeech.definitions[0].definition:'')}</div>
+     <div class="meaning">${k++,partsofspeech.definitions[1]?partsofspeech.definitions[1].definition:''}</div>
+     <div class="meaning">${k++,partsofspeech.definitions[2]?partsofspeech.definitions[2].definition:''}</div>
+     <div class="meaning">${k++,partsofspeech.definitions[3]?partsofspeech.definitions[3].definition:''}</div>
+     </div>
+     </div>
+    `
+    infdata.innerHTML=inf
     
-    
+  });
+  } catch(error) {
+    alert("Word not found")
+  }
 }
-async function fun2(){
-    let urldata= await fetch("https://aws.random.cat/meow")
-    let fetcheddata=await urldata.json()
-    try {
-        let imagedata=document.querySelector(".image")
-    imagedata.setAttribute("src",fetcheddata.file)
-    } catch (error) {
-        let videodata=document.querySelector(".video")
-        videodata.setAttribute("src",fetcheddata.file)
-    }
-    
-}
+
+
+
